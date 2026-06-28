274f92f22f7f1f47eccb30f6e3572a111b73ac67
kaviniamandimav123 <kaviniamandimav123@gmail.com>
2026-06-28 20:51:00 +0530

    Changed the print line text

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# Predict and evaluate
+# Predict and evaluate and printing it
 y_pred = model.predict(X_test)
-print("Accuracy of the prediction:", accuracy_score(y_test, y_pred))
+print("Accuracy of the prediction is:", accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
